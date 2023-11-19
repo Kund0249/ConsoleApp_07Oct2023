fe79c1bcecf4a9e8e7ad4dd5fa82dc1fc0f65550
Kund0249 <k.kundan1999@gmail.com>
2023-11-19 13:06:48 +0530

    Delegated and FileIO

---
+++ ConsoleApp_07Oct2023/DelegateExample/DelegateDemo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_07Oct2023.DelegateExample
+{
+    public delegate void MyDel();
+    public delegate bool CustomeDel(int n);
+    class Abc
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 Invoked of class Abc");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method2 Invoked of class Abc");
+        }
+
+        public void PrintNumber(CustomeDel del)
+        {
+            List<int> list = new List<int>()
+            {
+                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+            };
+
+            foreach (int item in list)
+            {
+                if (del.Invoke(item))
+                    Console.WriteLine(item);
+            }
+        }
+    }
+
+    class PQR
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 Invoked of class PQR");
+        }
+
+    }
+    class DelegateDemo
+    {
+
+        //public static bool PrimeLogic(int n)
+        //{
+        //    //if (n > 10)
+        //    //    return true;
+        //    //return false;
+
+        //    for (int i = 2; i <= n / 2; i++)
+        //    {
+        //        if (n % i == 0)
+        //            return false;
+        //    }
+
+        //    return true;
+        //}
+
+        //public static bool EvenLogic(int n)
+        //{
+        //    if (n % 2 == 0)
+        //        return true;
+        //    return false;
+        //}
+        static void Main(string[] args)
+        {
+            Abc abc = new Abc();
+            //CustomeDel PrimeDel = new CustomeDel(PrimeLogic);
+            //Console.WriteLine("All Prime Numbers");
+            //abc.PrintNumber(PrimeDel);
+
+            //Console.WriteLine("All Even Numbers");
+            //CustomeDel EvenDel = new CustomeDel(EvenLogic);
+            //abc.PrintNumber(EvenDel);
+
+            Console.WriteLine("All Gereater then 10");
+            abc.PrintNumber(x => x > 10);
+            Console.WriteLine("All Even Number");
+            abc.PrintNumber(x => x % 2 == 0);
+
+            Console.WriteLine("All Odd Number");
+            abc.PrintNumber(x => x % 2 != 0);
+
+            //abc.Method2();
+
+            PQR pqr = new PQR();
+            //pqr.Method1();
+
+            //MyDel del = new MyDel(abc.Method1);
+            //del += abc.Method2;
+            //del += pqr.Method1;
+
+            //del.Invoke();
+
+        }
+    }
+}
---
+++ ConsoleApp_07Oct2023/FileHandling/Logger.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp_07Oct2023.FileHandling
+{
+
+    //Foleder : ErrorLog
+    //File : Log_19112023
+    class Logger
+    {
+        static void Main(string[] args)
+        {
+            string DirectoryPath = @"C:\Users\Admin\Desktop\Tets";
+            string filePath = "Log";
+
+            filePath = Path.Combine(DirectoryPath, filePath+"_"+ DateTime.Now.ToString("ddMMyyyy")+".txt");
+
+            Console.WriteLine(filePath);
+            //Console.WriteLine(DateTime.Now);
+            //Console.WriteLine(DateTime.Now.ToShortDateString());
+            //Console.WriteLine(DateTime.Now.ToLongDateString());
+            //Console.WriteLine(DateTime.Now.ToString("ddMMyyyy"));
+
+            if (File.Exists(filePath))
+            {
+                //StreamWriter sw = new StreamWriter(filePath,true);
+                //sw.WriteLine("This is some text");
+                //sw.Close();
+                //Console.WriteLine("Writing compelted!");
+
+                //StreamReader SR = new StreamReader(filePath);
+                
+                //Console.WriteLine(SR.ReadToEnd());
+                
+                
+            }
+
+            //if (Directory.Exists(DirectoryPath))
+            //{
+            //    filePath = Path.Combine(DirectoryPath, filePath);
+            //    if (File.Exists(filePath))
+            //    {
+            //        File.Delete(filePath);
+            //        Console.WriteLine("File Deleted : " + Path.GetFileName(filePath));
+            //    }
+            //    //string[] files = Directory.GetFiles(DirectoryPath);
+            //    //Console.WriteLine($"---------{Path.GetDirectoryName(DirectoryPath)}------------");
+            //    //foreach (var item in files)
+            //    //{
+            //    //    Console.WriteLine(Path.GetFileName(item));
+            //    //}
+            //    //Console.WriteLine("------------End-------------");
+            //}
+
+            //if (Directory.Exists(DirectoryPath))
+            //{
+            //    Console.WriteLine("Test Directory available");
+
+            //    filePath = Path.Combine(DirectoryPath, filePath);
+               
+            //    if (File.Exists(filePath))
+            //    {
+            //        Console.WriteLine("File available");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("File Not Available");
+            //        File.Create(filePath);
+            //        Console.WriteLine("File Created");
+            //    }
+                   
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Test Directory not available");
+            //    Directory.CreateDirectory(DirectoryPath);
+            //    Console.WriteLine("Directory Created : " + DirectoryPath);
+            //}
+
+          
+        }
+    }
+}
